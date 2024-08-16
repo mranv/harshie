cb34bb64554ede5d3bc7afa480be864b80af9c99
mranv <iamanubhavgain@gmail.com>
2024-08-17 02:54:48 +0530

    feat: Add FeaturesSectionDemo component to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import FeaturesSectionDemo from "@/components/blocks/features-section-demo-1";
 import AppleCardsCarouselDemo from "@/components/example/apple-cards-carousel-demo-2";
 import GlobeDemo from "@/components/example/globe-demo";
 
@@ -6,6 +7,7 @@ export default function Home() {
     <>
       <GlobeDemo />
       <AppleCardsCarouselDemo />
+      <FeaturesSectionDemo />
     </>
   );
 }
