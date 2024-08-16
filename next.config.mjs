45994b7659cd8e3b23ac95304223563d3a422eb3
mranv <iamanubhavgain@gmail.com>
2024-08-17 02:12:30 +0530

    chore: Add font loader for Google fonts in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,11 @@ const nextConfig = {
     images: {
       domains: ["raw.githubusercontent.com", "images.unsplash.com"],
     },
+    experimental: {
+      fontLoaders: [
+        { loader: '@next/font/google', options: { subsets: ['latin'] } },
+      ],
+    },
   };
   
   export default nextConfig;
