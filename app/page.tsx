e01877db8854ec5c89b06095a0fd1ca4fd9d50ed
mranv <iamanubhavgain@gmail.com>
2024-08-17 02:38:30 +0530

    chore: Remove LampDemo component and update GlobeDemo message

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
 import AppleCardsCarouselDemo from "@/components/example/apple-cards-carousel-demo-2";
-
-
 import GlobeDemo from "@/components/example/globe-demo";
-import LampDemo from "@/components/example/lamp-demo";
 
 export default function Home() {
   return (
     <>
-      <LampDemo />
       <GlobeDemo />
       <AppleCardsCarouselDemo />
     </>
--- next.config.mjs
+++ next.config.mjs
@@ -3,11 +3,6 @@ const nextConfig = {
     images: {
       domains: ["raw.githubusercontent.com", "images.unsplash.com"],
     },
-    experimental: {
-      fontLoaders: [
-        { loader: '@next/font/google', options: { subsets: ['latin'] } },
-      ],
-    },
   };
   
   export default nextConfig;
