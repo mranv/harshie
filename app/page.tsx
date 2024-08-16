2ac7410a045d43db13ef2d20b1fbd1c6dea175c8
mranv <iamanubhavgain@gmail.com>
2024-08-17 01:03:56 +0530

    chore: Update page.tsx to use new components
    feat: Add useOutsideClick hook for handling outside clicks

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
-import LampDemo from "@/components/example/lamp-demo";
+import AppleCardsCarouselDemo from "@/components/example/apple-cards-carousel-demo-2";
+import FloatingDockDemo from "@/components/example/floating-dock-demo";
+import FloatingNavDemo from "@/components/example/floating-navbar-demo";
+import GlobeDemo from "@/components/example/globe-demo";
 
 export default function Home() {
   return (
     <>
-      <LampDemo />
+      <FloatingNavDemo />
+      <GlobeDemo />
+      <AppleCardsCarouselDemo />
     </>
   );
 }
